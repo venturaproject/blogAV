83559479279f58230c6eef1f723f20ab82cf9e5e
AV Jimenez <antonioventura.j@gmail.com>
2021-10-04 20:18:38 +0200

    new release

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -21,25 +21,9 @@ const Footer = () => {
       }}
     >
       <div>
-        &copy; {new Date().getFullYear()} by {siteTitle}. All derecho reserved.
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
       </div>
-      <div>
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog"
-        >
-          Theme
-        </Link>
-        {` `}
-        by
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Theme"
-        >
-          LekoArts
-        </Link>
-      </div>
+
     </footer>
   )
 }
